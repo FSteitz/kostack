209b40bc734fd644676ddbde751a70a0f74fe45a
Florian Steitz <florian@fsteitz.com>
2020-07-11 17:59:39 +0200

    Convert project to Kotlin

--- src/main/java/com/github/fsteitz/kostack/Main.kt
+++ src/main/java/com/github/fsteitz/kostack/Main.kt
@@ -1,140 +1,145 @@
-package com.github.fsteitz.kostack;
+package com.github.fsteitz.kostack
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.util.ArrayList
+import java.util.List
+import java.util.function.Consumer
 
-public class Main {
+object Main {
 
-  private static final int DUMP_COUNT = 6;                      // Iterationen
-  private static final int DUMP_DELAY_S = 5;                    // In Sekunden
-  private static final int DUMP_DELAY_MS = DUMP_DELAY_S * 1000; // In Millisekunden
+  private const val DUMP_COUNT = 6                        // Iterationen
+  private const val DUMP_DELAY_S = 5                      // In Sekunden
+  private const val DUMP_DELAY_MS = DUMP_DELAY_S * 1000   // In Millisekunden
 
-  private static final String PROCESS_NAME_PART = "SHD ECORO";
-  private static final String DUMP_FILE_PATTERN = "\\thread_%s_%s__%s.dump";
+  private const val PROCESS_NAME_PART = "SHD ECORO"
+  private const val DUMP_FILE_PATTERN = "\\thread_%s_%s__%s.dump"
 
-  public static void main(String[] args) {
-    if (args.length == 0) {
-      System.err.println("FEHLER: Es wurden keine Programmparameter uebergeben");
-      System.exit(-1);
-    } else if (args.length != 2) {
-      System.err.println("FEHLER: Programmparameter sind ungültig");
-      System.exit(-1);
+  @JvmStatic
+  fun main(args: Array<String>) {
+    if (args.size == 0) {
+      System.err.println("FEHLER: Es wurden keine Programmparameter uebergeben")
+      System.exit(-1)
+    } else if (args.size != 2) {
+      System.err.println("FEHLER: Programmparameter sind ungültig")
+      System.exit(-1)
     }
 
-    createThreadDumps(args[0], args[1]);
+    createThreadDumps(args[0], args[1])
   }
 
-  private static void createThreadDumps(String jdkHome, String dumpFileBasePath) {
-    System.out.println("JDK-Home: " + jdkHome);
+  private fun createThreadDumps(jdkHome: String, dumpFileBasePath: String) {
+    println("JDK-Home: $jdkHome")
 
     try {
-      String jstackPattern = jdkHome + "\\jstack -l %s";
-      List<String> ecoroPIDs = getEcoroPIDs();
+      val jstackPattern = "$jdkHome\\jstack -l %s"
+      val ecoroPIDs = getEcoroPIDs()
 
-      if (ecoroPIDs.size() == 0) {
-        System.out.println("FEHLER: Es konnte kein aktiver Prozess von SHD ECORO ermittelt werden. Es wurden keine ThreadDumps erstellt!");
-        System.exit(-1);
+      if (ecoroPIDs.size == 0) {
+        println("FEHLER: Es konnte kein aktiver Prozess von SHD ECORO ermittelt werden. Es wurden keine ThreadDumps erstellt!")
+        System.exit(-1)
       }
 
-      System.out.println("Alle ermittelten PIDs von SHD ECORO: " + ecoroPIDs.toString());
+      println("Alle ermittelten PIDs von SHD ECORO: $ecoroPIDs")
 
-      for (String pid : ecoroPIDs) {
-        createThreadDumps(pid, jstackPattern, dumpFileBasePath);
+      for (pid in ecoroPIDs) {
+        createThreadDumps(pid, jstackPattern, dumpFileBasePath)
       }
-    } catch (Exception e) {
-      System.err.println("FEHLER: Die Datei konnte nicht generiert werden");
-      e.printStackTrace();
+    } catch (e: Exception) {
+      System.err.println("FEHLER: Die Datei konnte nicht generiert werden")
+      e.printStackTrace()
     }
   }
 
-  private static void createThreadDumps(String pid, String jstackPattern, String dumpFileBasePath) throws IOException, InterruptedException {
-    System.out.println("Erzeuge insgesamt " + DUMP_COUNT + " ThreadDumps fuer PID '" + pid + "'");
+  @Throws(IOException::class, InterruptedException::class)
+  private fun createThreadDumps(pid: String, jstackPattern: String, dumpFileBasePath: String) {
+    println("Erzeuge insgesamt $DUMP_COUNT ThreadDumps fuer PID '$pid'")
 
     // Zähler beginnt bei 1 um nutzerfreundlicher zu sein
-    for (int i = 1; i <= DUMP_COUNT; i++) {
-      createThreadDump(pid, i, jstackPattern, dumpFileBasePath);
-      System.out.println("ThreadDump " + i + " von " + DUMP_COUNT + " erzeugt. In " + DUMP_DELAY_S + " Sek. wird der naechste erzeugt");
-      Thread.sleep(DUMP_DELAY_MS);
+    for (i in 1..DUMP_COUNT) {
+      createThreadDump(pid, i, jstackPattern, dumpFileBasePath)
+      println("ThreadDump $i von $DUMP_COUNT erzeugt. In $DUMP_DELAY_S Sek. wird der naechste erzeugt")
+      Thread.sleep(DUMP_DELAY_MS.toLong())
     }
 
-    System.out.println("FERTIG: Alle ThreadDumps wurden erzeugt!");
+    println("FERTIG: Alle ThreadDumps wurden erzeugt!")
   }
 
-  private static void createThreadDump(String pid, int dumpIndex, String jstackPattern, String dumpFileBasePath) throws IOException {
-    exec(String.format(jstackPattern, pid), stdin -> {
+  @Throws(IOException::class)
+  private fun createThreadDump(pid: String, dumpIndex: Int, jstackPattern: String, dumpFileBasePath: String) {
+    exec(String.format(jstackPattern, pid), Consumer exec@{ stdin: BufferedReader ->
       try {
-        String line;
-        Path dumpFile = Path.of(String.format(dumpFileBasePath + DUMP_FILE_PATTERN, pid, System.currentTimeMillis(), dumpIndex));
-        System.out.println("ThreadDump wird erzeugt: " + dumpFile.toString());
+        var line: String
+        val dumpFile = Path.of(String.format(dumpFileBasePath + DUMP_FILE_PATTERN, pid, System.currentTimeMillis(), dumpIndex))
+        println("ThreadDump wird erzeugt: $dumpFile")
 
         if (!dumpFile.toFile().createNewFile()) {
-          System.out.println("ThreadDump fuer PID '" + pid + "' konnte nicht erzeugt werden");
-          return;
+          println("ThreadDump fuer PID '$pid' konnte nicht erzeugt werden")
+          return@exec
         }
 
-        while ((line = stdin.readLine()) != null) {
-          Files.writeString(dumpFile, line + '\n', StandardOpenOption.APPEND);
+        while (stdin.readLine().also { line = it } != null) {
+          Files.writeString(dumpFile, line + '\n', StandardOpenOption.APPEND)
         }
 
-        stdin.close();
-      } catch (IOException e) {
-        System.err.println("FEHLER: ThreadDump konnte nicht erzeugt werden");
-        e.printStackTrace();
+        stdin.close()
+      } catch (e: IOException) {
+        System.err.println("FEHLER: ThreadDump konnte nicht erzeugt werden")
+        e.printStackTrace()
       }
-    });
+    })
   }
 
-  private static List<String> getEcoroPIDs() throws IOException {
-    List<String> pids = new ArrayList<>();
+  @Throws(IOException::class)
+  private fun getEcoroPIDs(): MutableList<String> {
+    val pids: MutableList<String> = ArrayList()
 
-    System.out.println("Ermittle PIDs von SHD ECORO");
-    exec("tasklist /v /fo csv", stdin -> {
-      String line;
+    println("Ermittle PIDs von SHD ECORO")
+    exec("tasklist /v /fo csv", Consumer { stdin: BufferedReader ->
+      var line: String
 
       try {
-        while ((line = stdin.readLine()) != null) {
-          String[] columns = line.split(",");
+        while (stdin.readLine().also { line = it } != null) {
+          val columns: Array<String?> = line.split(",").toTypedArray()
 
-          if (columns.length < 2) {
-            System.err.println("FEHLER: Format von 'tasklist' ist ungueltig");
-          } else if (columns[columns.length - 1] != null && columns[columns.length - 1].contains(PROCESS_NAME_PART)) {
-            String pid = columns[1].replaceAll("\"", "");
+          if (columns.size < 2) {
+            System.err.println("FEHLER: Format von 'tasklist' ist ungueltig")
+          } else if (columns[columns.size - 1] != null && columns[columns.size - 1]!!.contains(PROCESS_NAME_PART)) {
+            val pid = columns[1]!!.replace("\"".toRegex(), "")
 
-            System.out.println("PID von SHD ECORO ermittelt: " + pid);
-            pids.add(pid);
+            println("PID von SHD ECORO ermittelt: $pid")
+            pids.add(pid)
           }
         }
-      } catch (IOException e) {
-        System.err.println("FEHLER: ThreadDump konnte nicht erzeugt werden");
-        e.printStackTrace();
+      } catch (e: IOException) {
+        System.err.println("FEHLER: ThreadDump konnte nicht erzeugt werden")
+        e.printStackTrace()
       }
-    });
+    })
 
-    return pids;
+    return pids
   }
 
-  private static void exec(String command, Consumer<BufferedReader> stdinConsumer) throws IOException {
-    Process process = Runtime.getRuntime().exec(command);
-    BufferedReader stdin = new BufferedReader(new InputStreamReader(process.getInputStream()));
-    BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-    String line;
+  @Throws(IOException::class)
+  private fun exec(command: String, stdinConsumer: Consumer<BufferedReader>) {
+    val process = Runtime.getRuntime().exec(command)
+    val stdin = BufferedReader(InputStreamReader(process.inputStream))
+    val stderr = BufferedReader(InputStreamReader(process.errorStream))
+    var line: String?
 
     // Read the output from the command
-    stdinConsumer.accept(stdin);
-    stdin.close();
+    stdinConsumer.accept(stdin)
+    stdin.close()
 
     // Read any errors from the attempted command
-    while ((line = stderr.readLine()) != null) {
-      System.out.println(line);
+    while (stderr.readLine().also { line = it } != null) {
+      println(line)
     }
 
-    stderr.close();
+    stderr.close()
   }
 }
